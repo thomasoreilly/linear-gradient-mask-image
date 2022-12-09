9be29f418d4d2b966811da8c526677140064de2f
thomasoreilly <thomasreilly9@gmail.com>
2022-12-09 10:35:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# linear-gradient-mask-image
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-1kv4c4)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1><div class="masked-overflow">If you want more control over the direction of the gradient, you can define an angle, instead of the predefined directions (to bottom, to top, to right, to left, to bottom right, etc.). A value of 0deg is equivalent to "to top". A value of 90deg is equivalent to "to right". A value of 180deg is equivalent to "to bottom".JS Starter If you want more control over the direction of the gradient, you can define an angle, instead of the predefined directions (to bottom, to top, to right, to left, to bottom right, etc.). A value of 0deg is equivalent to "to top". A value of 90deg is equivalent to "to right". A value of 180deg is equivalent to "to bottom".JS Starter</div></h1>`;
